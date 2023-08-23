da054dffef48ffaa57b35d00fe86839600e707a5
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2023-08-22 23:40:13 -0300

    fix: function to add at end

--- estrutura-de-dados-algoritmos/src/vector_array/Main.java
+++ estrutura-de-dados-algoritmos/src/vector_array/Main.java
@@ -4,6 +4,13 @@ public class Main {
 
     public static void main(String[] args) {
         Vector vetor = new Vector(5);
-
+        vetor.addToEnd("Elemento 1");
+        vetor.addToEnd("Elemento 2");
+        vetor.addToEnd("Elemento 3");
+        vetor.addToEnd("Elemento 4");
+        vetor.addToEnd("Elemento 5");
+        vetor.addToEnd("Elemento 6");
+        vetor.print();
+        System.out.println(vetor.getSize());
     }
 }
--- estrutura-de-dados-algoritmos/src/vector_array/Vector.java
+++ estrutura-de-dados-algoritmos/src/vector_array/Vector.java
@@ -3,18 +3,31 @@ package vector_array;
 public class Vector {
 
     private String[] elements;
+    private int capacity;
+    private int size;
 
-    public Vector(int size) {
-        this.elements = new String[size];
+    public Vector(int capacity) {
+        this.capacity = capacity;
+        this.elements = new String[capacity];
     }
 
-    public void add(String element) {
-        for (int i = 0; i < this.elements.length; i++) {
-            if (this.elements[i] == null) {
-                this.elements[i] = element;
-                break;
-            }
+    public void addToEnd(String element) {
+        if (getSize() < capacity) {
+            this.elements[getSize()] = element;
+            this.size++;
         }
     }
 
+    public void print() {
+        System.out.print("[");
+        for (int i = 0; i < getSize(); i++) {
+            System.out.print(this.elements[i]);
+            if (i == (getSize() - 1)) System.out.println("]");
+            else System.out.print(", ");
+        }
+    }
+
+    public int getSize() {
+        return this.size;
+    }
 }
