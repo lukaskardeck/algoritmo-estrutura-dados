f9acdfce6ed8897e42bb6c07bcf0765ba9f3f48b
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2023-08-23 13:27:08 -0300

    getByIndex() and Find()

--- estrutura-de-dados-algoritmos/src/vector_array/Main.java
+++ estrutura-de-dados-algoritmos/src/vector_array/Main.java
@@ -3,14 +3,17 @@ package vector_array;
 public class Main {
 
     public static void main(String[] args) {
-        Vector vetor = new Vector(5);
+
+        Vector vetor = new Vector(2);
+
         vetor.addToEnd("Elemento 1");
         vetor.addToEnd("Elemento 2");
         vetor.addToEnd("Elemento 3");
-        vetor.addToEnd("Elemento 4");
-        vetor.addToEnd("Elemento 5");
-        vetor.addToEnd("Elemento 6");
-        vetor.print();
+
+        System.out.println(vetor);
+
         System.out.println(vetor.getSize());
+//        System.out.println(vetor.getByIndex(3));
+        System.out.println(vetor.find("Elemento 3"));
     }
 }
--- estrutura-de-dados-algoritmos/src/vector_array/Vector.java
+++ estrutura-de-dados-algoritmos/src/vector_array/Vector.java
@@ -1,5 +1,7 @@
 package vector_array;
 
+import java.util.Arrays;
+
 public class Vector {
 
     private String[] elements;
@@ -11,23 +13,50 @@ public class Vector {
         this.elements = new String[capacity];
     }
 
+    // Adiciona um elemento no final da estrutura
     public void addToEnd(String element) {
-        if (getSize() < capacity) {
+        if (getSize() < this.capacity) {
             this.elements[getSize()] = element;
             this.size++;
         }
-    }
-
-    public void print() {
-        System.out.print("[");
-        for (int i = 0; i < getSize(); i++) {
-            System.out.print(this.elements[i]);
-            if (i == (getSize() - 1)) System.out.println("]");
-            else System.out.print(", ");
+        else {
+            throw new RuntimeException("O vetor já está preenchido");
         }
     }
 
+    // Retorna um elmento pelo seu índice na estrutura
+    public String getByIndex(int index) {
+        if (index < 0 || index >= getSize()) {
+            throw new IllegalArgumentException("Invalid index");
+        }
+
+        return this.elements[index];
+    }
+
+    // Realiza uma busca sequencial do elemento e retorna o seu indice. No caso do elemento não ser encontrado, retornará -1
+    public Integer find(String element) {
+        for (int i = 0; i < getSize(); i++) {
+            if (this.elements[i].equals(element)) return i;
+        }
+
+        return -1;
+    }
+
+    // Retorna a quantidade de elementos presentes na estrutura
     public int getSize() {
         return this.size;
+    }
+    
+    @Override
+    public String toString() {
+        StringBuilder sb = new StringBuilder();
+        sb.append("[");
+        for (int i = 0; i < getSize(); i++) {
+            sb.append(this.elements[i]);
+            if (i != (getSize() - 1)) sb.append(", ");
+        }
+        sb.append("]");
+
+        return sb.toString();
     }
 }
