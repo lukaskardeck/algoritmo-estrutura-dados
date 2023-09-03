056efa6ec5aa6c8615d2dfa3ea1c34ebe7f7f2a0
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2023-09-03 20:12:05 -0300

    InsertionSort implementation

--- estrutura-de-dados-algoritmos/src/sorting_algorithms/ExampleRun.java
+++ estrutura-de-dados-algoritmos/src/sorting_algorithms/ExampleRun.java
@@ -10,8 +10,13 @@ public class ExampleRun {
 
     public static void main(String[] args) {
         int[] nums = {5, 7, 2, 1, 8};
+        int[] nums2 = {5, 8, 3, 1, 4};
         BubbleSort.bubbleSort(nums);
+        InsertionSort.insertionSort(nums2);
+
         printArray(nums);
+
+
     }
 
 }
