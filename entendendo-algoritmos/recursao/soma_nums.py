6adf6e8c6edc86be73ae5d81ef28bbbb6d26bba1
lukaskardeck <92531036+lukaskardeck@users.noreply.github.com>
2023-12-21 20:43:39 -0300

    recursao

---
+++ entendendo-algoritmos/recursao/soma_nums.py
@@ -0,0 +1,18 @@
+# somar números em uma lista usando 'loop for'
+def somar_num_loop(lista):
+    total = 0
+    for n in lista:
+        total += n
+    return total
+
+
+# somar números em uma lista usando recursão
+def somar_num_recursivo(lista):
+    if len(lista) == 0:
+        return 0
+    return lista[-1] + somar_num_recursivo(lista[:-1])
+
+
+
+print(somar_num_loop([1, 2, 3, 4, 5]))
+print(somar_num_recursivo([1, 2, 3, 4,5]))
